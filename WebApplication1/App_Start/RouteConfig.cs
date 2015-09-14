7f92470fe2808f72d116666f41b1ff34b00de157
wxkdesky <392928346@qq.com>
2015-09-15 00:14:51 +0800

    完成websocket调试

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "webSocketTest", action = "index", id = UrlParameter.Optional }
             );
         }
     }
